eab8711fbc6766c60bbb96d266e41b08dc53d1a9
Tatsuki-I <tatsuki@konokaze.jp>
2019-05-19 23:29:44 +0900

    commit

--- src/Sieve.hs
+++ src/Sieve.hs
@@ -3,15 +3,15 @@ module Sieve where
 import Data.Array.ST
 import Control.Monad
 
---sieve   :: Int -> MArray Int Bool
-sieve n =  runSTArray $ do arr <- newArray (0, n) True
-                           writeArray arr 0 False
-                           writeArray arr 1 False
-                           mapM_ (\i -> writeArray arr i False) [4, 6 .. n]
-                           mapM_ (\i -> readArray arr i >>= 
-                                        (flip when) (mapM_ (\j -> (writeArray arr j False))
-                                                           [i * i, i * (i + 2) .. n])) 
-                                 [3, 5 .. n]
-                           return arr
-                           --where sqn = (fromIntegral . ceiling . sqrt) n
+sieve n =  runSTArray $
+           do arr <- newArray (0, n) True
+              writeArray arr 0 False
+              writeArray arr 1 False
+              mapM_ (\i -> writeArray arr i False) [4, 6 .. n]
+              mapM_ (\i -> readArray arr i >>= 
+                           (flip when) (mapM_ (\j -> (writeArray arr j False))
+                                              [i * i, i * (i + 2) .. n])) 
+                    [3, 5 .. n]
+              return arr
+              --where sqn = (fromIntegral . ceiling . sqrt) n
 
