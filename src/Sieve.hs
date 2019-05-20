45f6f2f06e8528804b1eb9e0760bba13ccc2ec0f
Tatsuki-I <tatsuki.devel@gmail.com>
2019-05-20 11:39:35 +0900

    commit

--- src/Sieve.hs
+++ src/Sieve.hs
@@ -1,18 +1,21 @@
 module Sieve where
 
 import Data.Array.ST
+import Data.Array
 import Control.Monad
 
-sieve n =  runSTArray $
-           do arr <- newArray (0, n) True
-              writeArray arr 0 False
-              writeArray arr 1 False
-              mapM_ (\i -> writeArray arr i False) [4, 6 .. n]
-              mapM_ (\i -> readArray arr i >>= 
-                           flip when (mapM_ (\j -> writeArray arr j False)
-                                            [i * i, i * (i + 2) .. n])) 
-                    [3, 5 .. sqn]
-              return arr
-              where sqn :: (Integral a) => a
-                    sqn =  (ceiling . sqrt . fromIntegral) n
-
+sieve               :: (Integral a, Ix a) => a -> [a]
+sieve n
+    | n < 2     =  []
+    | otherwise =  map fst . filter snd . (drop 2 . assocs) $ runSTArray $
+                   do a <- newArray (0, n) True
+                      writeArray a 0 False
+                      writeArray a 1 False
+                      mapM_ (\i -> writeArray a i False) [4, 6 .. n]
+                      mapM_ (\i -> readArray a i >>= 
+                                   flip when (mapM_ (\j -> writeArray a j False)
+                                                    [i * i, i * (i + 2) .. n])) 
+                            [3, 5 .. sqn]
+                      return a
+                      where sqn :: (Integral a) => a
+                            sqn =  (ceiling . sqrt . fromIntegral) n
