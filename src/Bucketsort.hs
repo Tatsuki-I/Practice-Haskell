4da909d8a78c033b035943f8fee30b7b0cdf79c0
Tatsuki-I <tatsuki.devel@gmail.com>
2019-05-20 13:24:12 +0900

    commit

---
+++ src/Bucketsort.hs
@@ -0,0 +1,14 @@
+module Bucketsort where
+
+import Data.Array.ST
+import Data.Array
+
+bsort    :: (Ix a, Foldable t) => t a -> [a]
+bsort xs =  concatMap (\x -> replicate (snd x) (fst x)) .
+            assocs $ runSTArray $
+            do bucket <- newArray (minimum xs, maximum xs) 0
+               mapM_ (\i -> (+ 1) <$> readArray bucket i >>=
+                            writeArray bucket i)
+                     xs
+               return bucket
+
