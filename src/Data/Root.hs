7a270cb3f2bb76950c7bc4a57baf5babaaf5bbf2
Tatsuki-I <tatsuki.devel@gmail.com>
2019-06-26 13:48:09 +0900

    add plus operator

--- src/Data/Root.hs
+++ src/Data/Root.hs
@@ -1,10 +1,10 @@
 module Data.Root where
 
-import Data.Ratio
+import Data.Ratio      ((%), numerator)
 import Data.List       (group, partition)
 import Data.List.Split (chunksOf)
 
-data Rt = !Int :-/ !Integer
+data Rt = !Int :-/ !Rational
           deriving ( Eq )
 
 data Root' = Rational :* Rt
@@ -26,25 +26,45 @@ instance Show Root where
                     where f x Zero     = show x
                           f x (y :+ z) = show x ++ " + " ++ f y z
 
-(-/)     :: Int -> Integer -> Root'
-(-/) a b =  toRational n :* (a :-/ nb)
-            where (n, nb) = (product $ map head fs2, product $ concat (ts2 ++ ts1))
-                             where (fs1, ts1) = partition (\xs -> length xs >= a)
-                                                          $ (group . factorisation) b
-                                   (fs2, ts2) = partition (\xs -> length xs == a)
-                                                          $ concatMap (chunksOf a) fs1
-                  factorisation   :: Integer -> [Integer]
-                  factorisation 1 =  []
-                  factorisation x =  v : factorisation (x `div` v)
-                                     where v = factors x !! 1
-                                           factors :: Integer -> [Integer]
-                                           factors n = [x | x <- [1..n], n `mod` x == 0]
+instance Num Root where
+     a    + Zero     =  a
+     Zero + a        =  a
+     a    + (b :+ c) = (f a b) + c
+                       where f             :: Root -> Root' -> Root
+                             f Zero z      =  z :+ Zero
+                             f (rt1@(c1 :* r1) :+ rs) rt2@(c2 :* r2)
+                               | r1 == r2  =  ((c1 + c2) :* r1) :+ rs
+                               | otherwise =  rt1 :+ (f rs rt2)
 
-a :: Root'
-a =  2 -/ 4
+--
+(-/)     :: Int -> Rational -> Root
+a -/ b =  (toRational n :* (a :-/ (nb % 1))) :+ Zero
+          where b' = numerator b
+                (n, nb) = (product $ map head fs2, product $ concat (ts2 ++ ts1))
+                           where (fs1, ts1) = partition (\xs -> length xs >= a)
+                                                        $ (group . factorisation) b'
+                                 (fs2, ts2) = partition (\xs -> length xs == a)
+                                                        $ concatMap (chunksOf a) fs1
+                factorisation   :: Integer -> [Integer]
+                factorisation 1 =  []
+                factorisation x =  v : factorisation (x `div` v)
+                                   where v = factors x !! 1
+                                         factors :: Integer -> [Integer]
+                                         factors n = [x | x <- [1..n], n `mod` x == 0]
 
-b :: Root'
-b = 2 -/ 3
+mul :: Root -> Root -> Root
+_    `mul` Zero     =  Zero
+Zero `mul` _        =  Zero
+a    `mul` (b :+ c) = (f a b) + a `mul` c
+                      where f :: Root -> Root' -> Root
+                            f Zero z      =  z :+ Zero
+--                            f (rt1@(c1 :* (n1 :-/ r1)) :+ rs) rt2@(c2 :* (n2 :-/ r2))
+--                              | r1 == r2  =  ((c1 + c2) :* r1) :+ rs
+--                              | otherwise =  rt1 :+ (f rs rt2)
 
-c :: Root
-c =  a :+ (b :+ Zero)
+g :: Root' -> Root' -> Root
+g (c1 :* (n1 :-/ r1))
+  (c2 :* (n2 :-/ r2)) | n1 == n2 =  (n1 -/ (r1 *
+                                           (product $ replicate n1 c1) *
+                                           r2 *
+                                           (product $ replicate n1 c2)))
