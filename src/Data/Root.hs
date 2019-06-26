8930fa22ca4ccdb998b4d9f194b14a65d280536d
Tatsuki-I <tatsuki.devel@gmail.com>
2019-06-26 11:16:21 +0900

    update

--- src/Data/Root.hs
+++ src/Data/Root.hs
@@ -7,17 +7,26 @@ import Data.List.Split (chunksOf)
 data Rt = !Int :-/ !Integer
           deriving ( Eq )
 
-data Root = Rational :* Rt
+data Root' = Rational :* Rt
+             deriving ( Eq )
+
+data Root = Zero
+          | !Root' :+ !Root
             deriving ( Eq )
 
 instance Show Rt where
     show (a :-/ b) = show a ++ "-/ " ++ show b
 
-instance Show Root where
+instance Show Root' where
     show (a :* b) = "(" ++ show a ++ ") * " ++ show b
 
+instance Show Root where
+    show Zero = "Zero"
+    show (a :+ b) = f a b
+                    where f x Zero     = show x
+                          f x (y :+ z) = show x ++ " + " ++ f y z
 
-(-/)     :: Int -> Integer -> Root
+(-/)     :: Int -> Integer -> Root'
 (-/) a b =  toRational n :* (a :-/ nb)
             where (n, nb) = (product $ map head fs2, product $ concat (ts2 ++ ts1))
                              where (fs1, ts1) = partition (\xs -> length xs >= a)
@@ -30,3 +39,12 @@ instance Show Root where
                                      where v = factors x !! 1
                                            factors :: Integer -> [Integer]
                                            factors n = [x | x <- [1..n], n `mod` x == 0]
+
+a :: Root'
+a =  2 -/ 4
+
+b :: Root'
+b = 2 -/ 3
+
+c :: Root
+c =  a :+ (b :+ Zero)
