5a345f2f1d21da35118b2acc01d95749dde24a2a
Tatsuki-I <tatsuki.devel@gmail.com>
2019-07-15 13:16:30 +0900

    Commit

--- src/DiagonalisingListComprehensions.hs
+++ src/DiagonalisingListComprehensions.hs
@@ -1,9 +1,10 @@
 module DiagonalisingListComprehensions where
 
 --dlc       :: [a] -> [a] -> [a]
-dlc xs ys =  concatMap (uncurry zip) $ zip (f (length ys) xs) $ map reverse $ f (length xs) ys
+dlc xs ys =  (concatMap (uncurry zip) . zip (f (length ys) xs)) (map reverse $ f (length xs) ys)
+-- dlc xs ys =  concatMap (uncurry zip) $ zip (f (length ys) xs) $ map reverse $ f (length xs) ys
 --dlc xs ys =  concatMap (uncurry zip) $ zip (f ln xs) $ map reverse (f ln ys)
-             where ln =  min (length xs) (length ys)
+--             where ln =  min (length xs) (length ys)
 
 f      :: Int -> [a] -> [[a]]
 f n xs =  map (`take` xs) [1 .. n] ++ (if length xs < n
