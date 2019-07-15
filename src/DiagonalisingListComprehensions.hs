d5a04c25e6967a961f1dfb3100a62fa6099608a2
Tatsuki-I <tatsuki.devel@gmail.com>
2019-07-15 21:42:08 +0900

    update

--- src/DiagonalisingListComprehensions.hs
+++ src/DiagonalisingListComprehensions.hs
@@ -7,6 +7,7 @@ f      :: Int -> [a] -> [[a]]
 f n xs =  map (`take` xs) [1 .. n] ++ (if length xs < n
                                           then replicate (length xs - n) xs
                                           else []) ++ g n xs
-          where g            :: Int -> [a] -> [[a]]
-                g _ [x]      =  []
-                g n (x : xs) =  take n xs : g n xs
+
+g            :: Int -> [a] -> [[a]]
+g _ [x]      =  []
+g n (x : xs) =  take n xs : g n xs
